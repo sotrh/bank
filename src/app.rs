55f5eed4251e396edc6b606f9846d7519ce49c88
Benjamin Hansen <bhbenjaminhansen@gmail.com>
2024-03-02 16:02:39 -0700

    added player form

--- src/app.rs
+++ src/app.rs
@@ -2,10 +2,22 @@ use leptos::*;
 use leptos_meta::*;
 use leptos_router::*;
 
+#[derive(Debug, Clone)]
+pub struct Player {
+    name: String,
+    score: u32,
+}
+
+#[derive(Debug, Clone)]
+pub struct PlayerContext(ReadSignal<Vec<Player>>, WriteSignal<Vec<Player>>);
+
 #[component]
 pub fn App() -> impl IntoView {
     // Provides context that manages stylesheets, titles, meta tags, etc.
     provide_meta_context();
+
+    let (players, set_players) = create_signal(Vec::<Player>::new());
+    provide_context(PlayerContext(players, set_players));
 
     view! {
         // injects a stylesheet into the document <head>
@@ -30,13 +42,57 @@ pub fn App() -> impl IntoView {
 /// Renders the home page of your application.
 #[component]
 fn HomePage() -> impl IntoView {
-    // Creates a reactive value to update the button
-    let (count, set_count) = create_signal(0);
-    let on_click = move |_| set_count.update(|count| *count += 1);
+    let PlayerContext(players, set_players) = use_context().unwrap();
+    let (name, set_name) = create_signal("".to_owned());
+    let (error, set_error) = create_signal("");
+
+    let add_name = move |_| {
+        let name = name.get();
+
+        if error.with(|error| error.len() > 0) || name.len() == 0 {
+            return;
+        }
+
+        set_players.update(move |players| {
+            players.push(Player { name, score: 0 });
+        });
+    };
+
+    let name_input = move |ev| {
+        set_name(event_target_value(&ev));
+
+        // Maybe disable the button on error?
+        if players.with(|players| {
+            players
+                .iter()
+                .find(|player| player.name == name.get())
+                .is_some()
+        }) {
+            set_error.set("Player name already taken");
+        } else {
+            set_error.set("")
+        }
+    };
 
     view! {
-        <h1>"Welcome to Leptos!"</h1>
-        <button on:click=on_click>"Click Me: " {count}</button>
+        <h1>"Bank"</h1>
+        <input type="text"
+            on:input=name_input
+            prop:value=name
+        />
+        <button on:click=add_name>"Add Player"</button>
+        <div>{error}</div>
+        <ul>
+            <For
+                each=players
+                key=|player| player.name.clone()
+                children=move |player| {
+                    view! {
+                        <li>{player.name}</li>
+                    }
+                }
+            />
+        </ul>
     }
 }
 
